2b0bee31833fcb4a51ed24e98c7d2389fc7c3416
Marco Caberletti <marco.caberletti@gmail.com>
2016-10-14 13:29:50 +0200

    Add push image script Edit selenium hub start options

--- docker/push-image.sh
+++ docker/push-image.sh
@@ -4,8 +4,8 @@ set -xe
 
 DOCKER_REGISTRY_HOST=${DOCKER_REGISTRY_HOST:-"cloud-vm128.cloud.cnaf.infn.it"}
 
-image_name=italiangrid/iam-robot-testsuite:latest
-dest=${DOCKER_REGISTRY_HOST}/$image_name
+image_name=italiangrid/iam-robot-testsuite
+dest=${DOCKER_REGISTRY_HOST}/$image_name:latest
 	
 docker tag $image_name $dest
 docker push $dest
--- docker/selenium-grid/selenium_grid.sh
+++ docker/selenium-grid/selenium_grid.sh
@@ -8,6 +8,7 @@ function start(){
 	
 	if [ ! -z $DOCKER_NET_NAME ]; then
 		net_options="--net $DOCKER_NET_NAME"
+		node_options="-e HUB_PORT_4444_TCP_ADDR=selenium-hub -e HUB_PORT_4444_TCP_PORT=4444"
 	else
 		node_options="--link selenium-hub:hub"
 	fi
@@ -23,9 +24,9 @@ function start(){
 	sleep 5
 	
 	echo "Starting Chrome node..."
-	docker run -d $net_options $node_options -e HUB_PORT_4444_TCP_ADDR=selenium-hub --name node-chrome selenium/node-chrome
+	docker run -d $net_options $node_options --name node-chrome selenium/node-chrome
 	echo "Starting Firefox node..."
-	docker run -d $net_options $node_options -e HUB_PORT_4444_TCP_ADDR=selenium-hub --name node-firefox selenium/node-firefox
+	docker run -d $net_options $node_options --name node-firefox selenium/node-firefox
 }
 
 function stop(){
